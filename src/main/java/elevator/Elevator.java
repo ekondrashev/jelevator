0ee6f4fe3790b2a432a5962088ddb45674a7be67
Denis <dwaynewade0033@gmail.com>
2018-03-20 21:18:18 +0200

    doc

--- src/main/java/elevator/Elevator.java
+++ src/main/java/elevator/Elevator.java
@@ -2,6 +2,9 @@ package elevator;
 
 import java.util.Timer;
 
+/**
+ *
+ */
 public interface Elevator {
     int floor();
 
@@ -35,14 +38,37 @@ public interface Elevator {
         }
     }
 
+    /**
+     * Interface "Queue" we need to determine the queue.
+     */
     public interface Queue {
         String toString();
 
-        Queue queue();
+        /**
+         * Class "Position" we need to determine the position in the queue.
+         * Method "positionNumber" we need to know what position by number.
+         * Method "time" to calculate the time for how long we have to wait for the elevator, this method
+         * takes a queue because every single queue is a certain amount of time.
+         */
+        public class Position {
 
-        int position();
+            private Queue queue;
 
-        Timer time();
+
+            public Position(Queue queue) {
+                this.queue = queue;
+            }
+
+            int positionNumber() {
+                return positionNumber();
+            }
+
+            Timer time(Queue queue) {
+                return time(queue);
+            }
+
+
+        }
 
     }
 
